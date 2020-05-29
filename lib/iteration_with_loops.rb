004c69cd9e056058df5a9255ee4b37c10e26ca71
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-29 15:34:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,11 @@
 def find_even_values(src)
-  # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
-  #
-  # Output all even values in each nested array
-  index = 0
-while index < mother_array.length do
-  child_index = 0
-  while child_index < mother_array[index].length do
-    print(mother_array[index][child_index])
-    child_index += 1
+  even_numbers = []
+  for item in src do
+    for item in item do
+      if item % 2 == 0
+        even_numbers.push(item)
+      end
+    end
   end
-  index += 1
-end
-
+  return even_numbers
 end
