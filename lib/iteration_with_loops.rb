6c0ae655ea784f02ab5ce5ce2eb5f387fe5cda30
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-29 15:36:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,9 @@
 def find_even_values(src)
-  even_numbers = []
   for item in src do
     for item in item do
       if item % 2 == 0
-        even_numbers.push(item)
+        puts(item)
       end
     end
   end
-  return even_numbers
 end
